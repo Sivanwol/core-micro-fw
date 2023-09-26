bb3f6056c60f5ed7751309a714828ba2a5002446
Sivan Wolberg <sivan@wolberg.pro>
2023-09-26 04:02:32 +0300

    chore: update and make the simple test vs the db works as well fix gitignore issue

--- backend/Processor.test/Common/BaseTest.cs
+++ backend/Processor.test/Common/BaseTest.cs
@@ -11,20 +11,19 @@ public class BaseTest {
     public Mock<IDomainContext> Context { get; private set; }
     public Mock<IMediator> Mediator { get; private set; }
 
-    [SetUp]
     public void SetupTest(string testServiceName) {
         // Create log file and redirect output to it
         Log.Logger = ServicesLogger.GetLoggerTesting($"Test::${testServiceName}");
         MatchedDate = DateTimeOffset.Now;
         SystemClock.SetClock(new MockClock(MatchedDate));
         Context = MockTestHelper.SetupContext();
+        Mediator = new Mock<IMediator>();
     }
 
     public void ReloadData() {
         Context = MockTestHelper.SetupContext();
-        
     }
-    
+
     [TearDown]
     public void TearDown() {
         Log.CloseAndFlush();
--- backend/Processor.test/Users/UsersProcessTest.cs
+++ backend/Processor.test/Users/UsersProcessTest.cs
@@ -1,22 +1,21 @@
 using Processor.Handlers.User.List;
 using Processor.test.Common;
 
-namespace Processor.test.Users; 
+namespace Processor.test.Users;
 
 [TestFixture]
-public class UsersProcessTest: BaseTest {
-
+public class UsersProcessTest : BaseTest {
     [SetUp]
     public void Setup() {
         SetupTest("Users");
     }
 
     [Test]
-    public async Task GetUserList() {
+    public async Task GetUserListTest() {
         ReloadData();
         var requester = new ListUsersRequest();
         var handler = new ListUsersHandler(Mediator.Object, Context.Object);
-        var response= await handler.Handle(requester, CancellationToken.None);
+        var response = await handler.Handle(requester, CancellationToken.None);
         var exptendedEntities = MockTestHelper.GetUsers();
         Assert.That(response.Count, Is.EqualTo(exptendedEntities.Count));
         Assert.That(response[0].Id, Is.EqualTo(exptendedEntities[0].Id));
@@ -24,5 +23,4 @@ public class UsersProcessTest: BaseTest {
         Assert.That(response[1].Id, Is.EqualTo(exptendedEntities[1].Id));
         Assert.That(response[1].Auth0Id, Is.EqualTo(exptendedEntities[1].Auth0Id));
     }
-    
 }
