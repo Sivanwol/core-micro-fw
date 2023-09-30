e47442ab5b5904dee67642bf8bcc35f24d5aeae3
Sivan Wolberg <sivan@wolberg.pro>
2023-09-30 15:57:54 +0300

    chore: moving to .net 8.0 Rc

--- backend/Services/FrontApi/Bootstrap.cs
+++ backend/Services/FrontApi/Bootstrap.cs
@@ -54,6 +54,24 @@ public class Bootstrap {
             configuration.RegisterServicesFromAssemblyContaining(typeof(ListUsersRequest));
             configuration.RegisterServicesFromAssemblyContaining(typeof(CreateUserRequest));
         });
+        services.Configure<IdentityOptions>(options => {
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireNonAlphanumeric = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequiredLength = 6;
+            options.Password.RequiredUniqueChars = 1;
+
+            // Lockout settings.
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.AllowedForNewUsers = true;
+
+            // User settings.
+            options.User.AllowedUserNameCharacters =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+        });
         services.AddElasticsearch(Configuration);
         services.AddMassTransitExtension(Configuration, bus => { bus.AddConsumer<IndexUserConsumerHandler>(); });
         // health checks registration
